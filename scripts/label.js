64ee4a25421b357db6a6776cf1f59c676d6365d8
James Baicoianu <james_github@baicoianu.com>
2011-10-19 23:54:09 -0700

    Added label thing type

---
+++ scripts/label.js
@@ -0,0 +1,25 @@
+elation.extend("space.meshes.label", function(args) {
+  elation.space.thing.call( this, args);
+
+  this.createGeometry = function() {
+    var geometry = new THREE.TextGeometry( this.args.content.text, {
+      size: 50,
+      height: 5,
+      curveSegments: 6,
+
+      font: "helvetiker",
+      weight: "normal",
+      style: "normal",
+
+      bevelThickness: 2,
+      bevelSize: 1,
+      bevelEnabled: true
+    });                                                
+    this.createMesh(geometry, new THREE.MeshPhongMaterial({color: 0x666666}));
+  }
+  this.init();
+});
+elation.space.meshes.label.prototype = new elation.space.thing()
+elation.space.meshes.label.prototype.supr = elation.space.thing.prototype
+elation.space.meshes.label.prototype.constructor = elation.space.meshes.label;
+
