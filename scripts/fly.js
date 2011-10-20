1f81494479ca000a119161095de29286cc420b34
James Baicoianu <james_github@baicoianu.com>
2011-10-19 22:03:11 -0700

    Added fog, updated to with with three.js r46dev, added skydome, added TrackballControlls fallback when no object attached to camera, initial mouse picking support, use new physics engine when available

--- scripts/fly.js
+++ scripts/fly.js
@@ -10,19 +10,48 @@ elation.component.add('space.fly', {
     //this.camera = new THREE.FlyCamera({fov: 50, aspect: this.viewsize[0] / this.viewsize[1], movementSpeed: 200, domElement: this.container, near: 1, far: 1e4, rollSpeed: Math.PI / 6, dragToLook: true});
 
     this.scene = this.args.scene || new THREE.Scene();
+    this.scene.fog = new THREE.FogExp2(0xCCE8FF, 0.00008);
 
-    this.camera = new THREE.Camera( 50, this.viewsize[0] / this.viewsize[1], 1, 100000 );
+    this.camera = new THREE.PerspectiveCamera(50, this.viewsize[0] / this.viewsize[1], 1, 35000);
+    //this.camera = new THREE.OrthographicCamera(this.viewsize[0] / -2, this.viewsize[0] / 2, this.viewsize[1] / -2, this.viewsize[1] / 2, 0, 5000);
+    this.camera.position.z = 500;
+
+    this.controls = new THREE.TrackballControls( this.camera );
+    this.controls.rotateSpeed = -1.0;
+    this.controls.zoomSpeed = 4;
+    this.controls.panSpeed = 3;
+
+    this.controls.noZoom = false;
+    this.controls.noPan = false;
+
+    this.controls.staticMoving = true;
+    this.controls.dynamicDampingFactor = 0.3;
+    this.controls.keys = [ 65, 83, 68 ];
+
+    this.scene.add(this.camera);
 
     // TODO - light should just be a property of the sun...
-    this.lights['white'] = new THREE.SpotLight( 0xfffffff, 1, 200000);
+    this.lights['white'] = new THREE.SpotLight( 0xffffff, 1, 50000);
     this.lights['white'].position.x = -10000;
     this.lights['white'].position.y = 10000;
-    this.lights['white'].position.z = -10000;
+    this.lights['white'].position.z = 10000;
     this.lights['white'].castShadow = true;
-    this.scene.addLight(this.lights['white']);
+    this.scene.add(this.lights['white']);
+/*
+    this.lights['white2'] = new THREE.PointLight( 0xaaaaaa, 1, 500000);
+    this.lights['white2'].position.x = 10000;
+    this.lights['white2'].position.y = 10000;
+    this.lights['white2'].position.z = 10000;
+    this.lights['white2'].castShadow = true;
+    this.scene.add(this.lights['white2']);
+*/
 
-    this.lights['ambient'] = new THREE.AmbientLight( 0x444444 );
-    this.scene.addLight(this.lights['ambient']);
+    this.sky = new THREE.Mesh(new THREE.SphereGeometry(30000, 10), new THREE.MeshBasicMaterial({ color: 0xB0E2FF}));
+    this.sky.flipSided = true;
+    this.scene.add(this.sky);
+
+    this.lights['ambient'] = new THREE.AmbientLight( 0x888888 );
+    this.scene.add(this.lights['ambient']);
 
     this.addObjects(this.args.sector, this.scene);
 
@@ -53,7 +82,20 @@ elation.component.add('space.fly', {
     this.stats.domElement.style.zIndex = 100;
     this.container.appendChild( this.stats.domElement );
 
+/*
+    (function(self) {
+      setTimeout(function() { self.loop(); }, 500);
+    })(this);
+*/
+    this.projector = new THREE.Projector();
+    this.mouse = [0,0];
+    elation.events.add(this.container, 'mousemove', this);
+
+    this.lastupdate = new Date().getTime();
     this.loop();
+    if (elation.utils.physics) {
+      setTimeout(function() { elation.utils.physics.system.start(); }, 5000);
+    }
   },
   getsize: function() {
     if (this.container) {
@@ -68,6 +110,11 @@ elation.component.add('space.fly', {
     })(this);
 
     var newsize = this.getsize();
+    var ts = new Date().getTime();
+
+    if (this.controls) {
+      this.controls.update();
+    }
     if (this.camera) {
       if (this.viewsize[0] != newsize[0] || this.viewsize[1] != newsize[1]) {
         this.viewsize = newsize;
@@ -75,7 +122,25 @@ elation.component.add('space.fly', {
         this.camera.aspect = this.viewsize[0] / this.viewsize[1];
         this.camera.updateProjectionMatrix();
       }
+      this.sky.position = this.camera.position;
+
+      var vector = new THREE.Vector3( this.mouse[0], -this.mouse[1], 0.5 );
+      this.projector.unprojectVector( vector, this.camera );
+
+      var ray = new THREE.Ray( this.camera.position, vector.subSelf( this.camera.position ).normalize() );
+
+      var c = THREE.Collisions.rayCastNearest( ray );
+      if( c ) {
+        var poi = ray.origin.clone().addSelf( ray.direction.clone().multiplyScalar(c.distance) );
+        //console.log("Found @ normal", c, poi);
+      }
+
+      if (elation.utils.physics) {
+        elation.utils.physics.system.iterate((ts - this.lastupdate) / 1000);
+      }
+
       this.renderer.render(this.scene, this.camera);
+      this.lastupdate = ts;
     }
     this.stats.update();
   },
@@ -83,7 +148,7 @@ elation.component.add('space.fly', {
     var currentobj = false;
     if (typeof elation.space.meshes[thing.type] == 'function') {
       currentobj = new elation.space.meshes[thing.type](thing.properties);
-      root.addChild(currentobj);
+      root.add(currentobj);
 
       console.log("Added new " + thing.type + " '" + thing.name + "'", currentobj);
       if (thing.things) {
@@ -96,10 +161,15 @@ elation.component.add('space.fly', {
     }
   },
   attachCameraToObject: function(thing) {
+    //this.camera = this.followcamera;
     this.camera.position = thing.position;
     this.camera.rotation = thing.rotation;
     this.camera.quaternion = thing.quaternion;
     this.camera.useTarget = false;
     this.camera.useQuaternion = true;
+  },
+  mousemove: function(ev) {
+    this.mouse[0] = ( ev.clientX / this.viewsize[0] ) * 2 - 1;
+    this.mouse[1] = ( ev.clientY / this.viewsize[1] ) * 2 - 1;
   }
 });
