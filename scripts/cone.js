e3b5993d3e43dd3b86bb32eb5ddf73558503ee7d
James Baicoianu <james_github@baicoianu.com>
2011-12-30 03:18:57 -0800

    Improved cone part placement

--- scripts/cone.js
+++ scripts/cone.js
@@ -4,14 +4,13 @@ elation.extend("space.meshes.cone", function(args) {
   this.parts = {};
 
   this.postinit = function() {
-console.log(this);
     this.createMesh();
   }
   this.createMesh = function() {
     this.parts['base'] = new THREE.Mesh(new THREE.CylinderGeometry(8, 8, 1), new THREE.MeshPhongMaterial({color: 0x222222}));
     this.parts['cone'] = new THREE.Mesh(new THREE.CylinderGeometry(2, 5, 20), new THREE.MeshPhongMaterial({color: 0xff6600}));
-    this.parts['cone'].position.y = 10.5;
-    this.parts['base'].position.y = -.5;
+    this.parts['cone'].position.y = 11;
+    this.parts['base'].position.y = .5;
     this.add(this.parts['base']);
     this.add(this.parts['cone']);
   }
