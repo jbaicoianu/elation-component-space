e288f03d5c29efacb67e0219c715d81169820125
James Baicoianu <james_github@baicoianu.com>
2012-01-12 19:13:51 -0800

    Pick cone material based on renderer type, set collider radius

--- scripts/cone.js
+++ scripts/cone.js
@@ -2,13 +2,15 @@ elation.extend("space.meshes.cone", function(args) {
   elation.space.thing.call(this, args);
   this.autocreategeometry = false;
   this.parts = {};
+  this.collisionradius = 8;
 
   this.postinit = function() {
     this.createMesh();
   }
   this.createMesh = function() {
-    this.parts['base'] = new THREE.Mesh(new THREE.CylinderGeometry(8, 8, 1), new THREE.MeshPhongMaterial({color: 0x222222}));
-    this.parts['cone'] = new THREE.Mesh(new THREE.CylinderGeometry(2, 5, 20), new THREE.MeshPhongMaterial({color: 0xff6600}));
+    var materialtype = (Detector.webgl ? THREE.MeshPhongMaterial : THREE.MeshBasicMaterial);
+    this.parts['base'] = new THREE.Mesh(new THREE.CylinderGeometry(8, 8, 1), new materialtype({color: 0x222222}));
+    this.parts['cone'] = new THREE.Mesh(new THREE.CylinderGeometry(2, 5, 20), new materialtype({color: 0xff6600}));
     this.parts['cone'].position.y = 11;
     this.parts['base'].position.y = .5;
     this.add(this.parts['base']);
