64a3cda21c2fb4a28c9fd87646cb65a8f2267efd
James Baicoianu <james_github@baicoianu.com>
2012-01-12 18:54:31 -0800

    Added thing edit mode for admin tool, mouse picking ignores transparent surfaces, fixed mouse picking for all angles

--- scripts/admin.js
+++ scripts/admin.js
@@ -7,7 +7,7 @@ elation.component.add('space.admin', {
     this.projectactive = true;
     this.projectfreq = 4;
     this.projecttime = 0;
-    this.objectstates = { hover: false, active: false };
+    this.objectstates = { hover: false, active: false, editing: false };
 
     this.inspector = elation.space.admin.inspector("space_admin_inspector", elation.html.create());
     this.container.appendChild(this.inspector.container);
@@ -24,14 +24,18 @@ elation.component.add('space.admin', {
       "move_backward": function(ev) { this.position.z += ev.value * 100; },
       "move_up": function(ev) { this.position.y += ev.value * 100; },
       "move_down": function(ev) { this.position.y -= ev.value * 100; },
+      "move": function(ev) { elation.space.admin('admin').moveThing(this, ev); }
     });
     elation.space.controls(0).addBindings("admin", {
       "keyboard_w": "move_forward",
       "keyboard_a": "move_left",
       "keyboard_s": "move_backward",
       "keyboard_d": "move_right",
-      "mouse_drag_delta_x": "move_left",
-      "mouse_drag_delta_y": "move_up"
+/*
+      "mouse_drag_x": "move_left",
+      "mouse_drag_y": "move_up"
+*/
+      "mouse_drag": "move"
     });
     
     this.updateThingtypes();
@@ -92,10 +96,27 @@ elation.component.add('space.admin', {
         if (thing == this.findThingParent(this.objectstates['active'])) {
           elation.html.addclass(item, 'state_active');
         }
+        if (thing == this.findThingParent(this.objectstates['editing'])) {
+          elation.html.addclass(item, 'state_editing');
+        }
         item.innerHTML = '<strong>' + thing.name + '</strong> <em>(' + thing.type + ')</em>';
+        var editlink = elation.html.create({tag: 'a', classname: 'space_admin_thing_edit'});
+        editlink.innerHTML = 'edit';
+        item.appendChild(editlink);
         if (thing.children.length > 0) {
           this.updateScene(thing, item);
         }
+        (function(self, thing) {
+          elation.events.add(item, "mouseover,click", function(ev) { 
+            
+            var objectstate = 'hover';
+            if (ev.type == 'click') {
+              objectstate = (elation.html.hasclass(ev.target, 'space_admin_thing_edit') ? 'editing' : 'active');
+            }
+            self.setObjectState(objectstate, thing);
+            ev.stopPropagation();
+          })
+        })(this, thing);
         list.appendChild(item);
       }
     }
@@ -118,21 +139,39 @@ elation.component.add('space.admin', {
     this.updateScene();
     this.updateThingtypes();
   },
+  editThing: function(thing) {
+    console.log('do what you will with ', thing);
+    if (!this.thingeditor) {
+      var container = elation.html.create({tag: 'div', classname: 'space_admin_thing_editor'});
+      container.innerHTML = 'yes!';
+      this.thingeditor = elation.space.admin.thing_editor('thingeditor', container, {direction: 'left'});
+      this.container.appendChild(container);
+    }
+    this.thingeditor.setActiveThing(thing);
+  },
   setObjectState: function(state, obj) {
     if (typeof obj != 'undefined' && this.objectstates[state] != obj) {
       var oldobj = this.objectstates[state];
       this.objectstates[state] = obj;
 
-      if (state == 'active') {
-        if (oldobj) {
-          elation.events.fire({type: "deselect", element: oldobj});
-        }
-        if (obj) {
-          elation.events.fire({type: "select", element: obj});
-          //elation.space.controls(0).activateContext("admin", obj);
-        } else {
-          //elation.space.controls(0).deactivateContext("admin");
-        }
+      switch (state) {
+        case 'active':
+          if (oldobj) {
+            elation.events.fire({type: "deselect", element: oldobj});
+          }
+          if (obj) {
+            elation.events.fire({type: "select", element: obj});
+          } else {
+          }
+          break;
+        case 'editing':
+          if (obj) {
+            this.editThing(obj);
+          } else {
+            this.editThing(false);
+          }
+          break;
+        
       }
       //console.log('Set ' + state + ' object:', obj);
       this.updateScene();
@@ -159,22 +198,59 @@ elation.component.add('space.admin', {
     this.mouse[0] = ( ev.clientX / viewsize[0] ) * 2 - 1;
     this.mouse[1] = ( ev.clientY / viewsize[1] ) * 2 - 1;
   },
-  projectMousePosition: function() {
+  projectMousePosition: function(mousepos) {
+    if (!mousepos) {
+      mousepos = this.mouse;
+    }
     var camera = elation.space.fly(0).camera;
     this.tmpvec.set(this.mouse[0], -this.mouse[1], .5);
     this.projector.unprojectVector( this.tmpvec, camera );
 
-    this.tmpray.origin = camera.position.clone();
-    //this.tmpray.origin = new THREE.Vector3(0,0,0);
-    //camera.matrix.multiplyVector3(this.tmpray.origin);
-    // FIXME - direction is wrong when camera is not at center...
+    this.tmpray.origin = new THREE.Vector3(0,0,0);
+    camera.matrixWorld.multiplyVector3(this.tmpray.origin);
     this.tmpray.direction = this.tmpvec.subSelf(this.tmpray.origin).normalize();
-    var intersects = this.tmpray.intersectScene(elation.space.fly(0).scene);
+    //console.log([this.tmpray.origin.x,this.tmpray.origin.y,this.tmpray.origin.z], [this.tmpray.direction.x,this.tmpray.direction.y,this.tmpray.direction.z]);
+    return this.tmpray.intersectScene(elation.space.fly(0).scene);
+  },
+  updateHover: function() {
+    var intersects = this.projectMousePosition();
     if (intersects.length > 0) {
-      this.setObjectState('hover', this.findThingParent(intersects[0].object));
+      var picked = intersects[0];
+      for (var i = 0; i < intersects.length; i++) {
+        var int = intersects[i];
+        if ((int.object.material instanceof THREE.MeshFaceMaterial && 
+            int.face.materialIndex &&
+            int.object.geometry.materials[int.face.materialIndex] && 
+            int.object.geometry.materials[int.face.materialIndex].transparent
+            ) || int.object.material.transparent) {
+        } else {
+          picked = intersects[i];
+          break;
+        }
+      }
+      this.setObjectState('hover', this.findThingParent(picked.object));
     } else {
       this.setObjectState('hover', false);
     }
+  },
+  moveThing: function(thing, mousepos) {
+    var intersects = this.projectMousePosition(mousepos);
+    var newpoint = false;
+    if (intersects.length > 0) {
+      for (var i = 0; i < intersects.length; i++) {
+        var hoverthing = this.findThingParent(intersects[i].object);
+        if (hoverthing != thing && hoverthing != this.objectstates['active']) {
+          newpoint = intersects[i].point.clone();
+          thing.position.copy(intersects[i].point);
+          break;
+        }
+      }
+    }
+    if (!newpoint) {
+      newpoint = new THREE.Vector3();
+      newpoint.add(this.tmpray.origin, this.tmpray.direction.multiplyScalar(100));
+    }
+    thing.position = newpoint;
   },
   mousemove: function(ev) {
     this.projectmoved = true;
@@ -182,13 +258,13 @@ elation.component.add('space.admin', {
 
     var now = ev.timeStamp;
     if (this.projectactive && now - this.projecttime > 1000 / this.projectfreq) {
-      this.projectMousePosition();
+      this.updateHover();
       this.projecttime = now;
       if (this.projecttimer) {
         clearTimeout(this.projecttimer);
       }
       (function(self) {
-        self.projecttimer = setTimeout(function() { self.projectMousePosition(); }, 1000 / self.projectfreq);
+        self.projecttimer = setTimeout(function() { self.updateHover(); }, 1000 / self.projectfreq);
       })(this);
     }
   },
@@ -196,7 +272,7 @@ elation.component.add('space.admin', {
     this.projectactive = false;
     this.projectmoved = false;
     this.updateMousePosition(ev);
-    this.projectMousePosition(ev);
+    this.updateHover(ev);
   },
   mouseup: function(ev) {
     this.projectactive = true;
@@ -209,7 +285,54 @@ elation.component.add('space.admin', {
   }
 });
 
+elation.component.add('space.admin.thing_editor', {
+  init: function() {
+    console.log('here I am');
+  },
+  setActiveThing: function(thing) {
+    if (thing) {
+      console.log('go for the thing', thing);
+      var foo = '<ul>';
+      if (thing && thing.properties) {
+        for (var k in thing.properties) {
+          foo += '<li>' + k + ' <ul>';
+          for (var k2 in thing.properties[k]) {
+            foo += '<li>' + k2 + ': ' + thing.properties[k][k2] + '</li>';
+          }
+          foo += '</ul>';
+        }
+      }
+      foo += '</ul>';
+      this.container.innerHTML = foo;
+      this.thing = thing;
+      
+      elation.space.controls(0).activateContext("admin", thing);
+
+      var fuh = elation.html.create({tag: 'a', classname: 'space_admin_thing_editor_close', content: 'close'});
+      this.container.appendChild(fuh);
+      elation.events.add(fuh, 'click', this);
+      this.container.style.display = 'block';
+    } else {
+      this.container.innerHTML = '';
+      this.container.style.display = 'none';
+    }
+  },
+  click: function(ev) {
+    elation.space.controls(0).deactivateContext("admin");
+    elation.space.admin('admin').setObjectState('editing', false);
+  }
+});
 elation.component.add('space.admin.inspector', {
   init: function() {
+  }
+});
+elation.extend('space.debug', new function() {
+  this.watching = {};
+    
+  this.init = function() {
+  }
+  this.watch = function(name, thing) {
+  }
+  this.update = function(ts) {
   }
 });
